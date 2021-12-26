2fab7ad5e61c42a52f68349feb2b926e4f4994fb
Hemant Gauhai <77909457+iamhemantgauhai@users.noreply.github.com>
2021-12-27 03:42:41 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f5ac286b-9d7d-4561-9042-26d4bc8965f5';
+export const MODAL_CLOSE_SEC = 2.5;
